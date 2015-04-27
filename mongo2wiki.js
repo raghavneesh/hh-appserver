8a7cc9d4f85761638a469aef2ffb0712c9d73cf4
Cherry G. Mathew <cherry@zyx.in>
2015-04-28 03:03:49 +0530

    Added signup and talk fields for the hillhacks dokuwiki.
    This is untested - pushing it out for testing.

--- mongo2wiki.js
+++ mongo2wiki.js
@@ -1,5 +1,8 @@
 //var mongoose = require('mongoose');
 //var ObjectId = mongoose.Schema.ObjectId;
+
+var moment = require('moment'); /* Date conversion */
+
 var http = require('http-request');
 
 module.exports = {
@@ -94,20 +97,121 @@ module.exports = {
 	    if (users) {
 		//Collating User Signup Data 
 		var wikidata = {};
+		var pickuptext;
+		
 		wikidata.useremail = users.email;
 		Booking.findOne({user: users._id}, function(err, bookings) {
-		    wikidata.usertext = "---- dataentry signup ----\n" + "email:" + users.email + "\nName:" + bookings.username;
 		    wikidata.usertitle = bookings.username;
+		    
+		    wikidata.usertext = "---- dataentry signup ----\n";
+		    wikidata.usertext += "type : signup\n";
+		    wikidata.usertext += "interestlevel : " +
+			(users.confirmed ?
+			 "Yes I am coming" :
+			 "I am coming, don't know when exactly yet") + "\n";
+		    
+		    wikidata.usertext += "accommodation :" +
+			(bookings.accommodation ?
+			 "Yes" : "No") + "\n";
+
+		    /* Arrival / Departure - there are no separate accoms dates */
+		    wikidata.user += "arrival : " + moment(bookings.arrival_date, 'yyyy-MM-DD') + "\n";
+		    wikidata.user += "departure : " + moment(bookings.depart_date, 'yyyy-MM-DD') + "\n";
+
+		    wikidata.usertext += "publication_hidden : Yes \n";
+		    
+		    wikidata.usertext += "email_hidden : " + users.email + "\n";
+		    pickuptext = "pickup_hidden : " +
+			(bookings.pickup ?
+			 "Yes" : "No") + "\n";
+						       
 		});
+		
+		Accommodation.findOne({user : users._id}, function(err, accommodation) {
+		    wiki.usertext += "family_hidden : " +
+			(accommodation.family ?
+			 "Yes" : "No" ) + "\n";
+
+		    wiki.usertext += "familydetails_hidden : " +
+			(accommodation.family_details ?
+			 "Yes" : "No" ) + "\n";
+
+		    wiki.usertext += "extra";
+		    wiki.usertext += "extrainfo_hidden    : --[fixed as not entered in app]";
+		    wiki.usertext += "mainconf_hidden     : --[fixed as not entered in app]"
+		    wiki.usertext += "preconf_hidden      : --[fixed as not entered in app]"
+		    wiki.usertext += "attendcode_hidden   : --[fixed as not entered in app]"
+		    wiki.usertext += "teachcode_hidden    : --[fixed as not entered in app]"
+		    wiki.usertext += "teachschool_hidden  : --[fixed as not entered in app]"
+		    wiki.usertext += "presentconf_hidden  : --[fixed as not entered in app]"
+
+		    wiki.usertext += "tent_hidden : " +
+			(accommodation.tent ?
+			 "Yes" : "No") + "\n";
+n		    
+		    wiki.usertext += "sleepingbag_hidden : " +
+			(accommodation.sleeping_bag ?
+			 "Yes" : "No") + "\n";
+
+		    wiki.usertext += "mat_hidden : " +
+			(accommodation.mat ?
+			 "Yes" : "No") + "\n";
+
+		    wiki.usertext += "pillow_hidden : " +
+			(accommodation.pillow ?
+			 "Yes" : "No") + "\n";
+		});
+
+
+		Pickup.findOne({user : users._id}, function(err, pickup) {
+		    wiki.usertext += pickuptext;
+		    wiki.usertext += "pickuploc_hidden : " + pickup.location + "\n";
+
+		    wiki.usertext += "pickupdate_hidden : " +
+			moment(pickup.depart_date, 'yyyy-MM-DD') + "\n";
+		    wiki.usertext += "pickupseats_hidden : " +
+			pickup.seats + "\n";
+		    wiki.usertext += "pickuptime_hidden : " +
+			pickup.time + "\n";
+		    
+		});
+
 		
 		//Extracting User Talks Data from MongoDB
 		Talks.find({user: users._id}, function(err, talks) {
 		    if (err) return callback(err,null);
 		    for (var prop in talks) {
-			//Collating User Talks Data 
-			wikidata.talktext = "---- dataentry signup ----\n" + "type:" + talks[prop].type + "\ntitle:" + talks[prop].title + "\neventtype:" + talks[prop].event + "\nduration:" + talks[prop].duration;
+			//Collating User Talks Data
 			wikidata.talktitle = talks[prop].title;
 			wikidata.talktype = talks[prop].type;
+			
+			wikidata.talktext = "---- dataentry signup ----\n";
+			wikidata.talktext += "type : " + talks[prop].type + "\n";
+			wikidata.talktext += "title : " + talks[prop].title + "\n";
+			wikidata.talktext += "eventtype : " + talks[prop].event + "\n";
+			wikidata.talktext += "duration : " + talks[prop].duration;
+			wikidata.talktext += "other_hidden : No\n";
+			wikidata.talktext += "othname_hidden : No\n";
+			wikidata.talktext += "projector_hidden : " +
+			    (talks[prop].needsProjector ?
+			     "Yes" : "No") + "\n";
+			
+			wikidata.talktext += "tool_hidden : " +
+			    (talks[prop].needsTools ?
+			     "Yes, I need tools" :
+			     "No, I don't need any tools") + "\n";
+
+			wikidata.talktext += "toolSpec_hidden : N.A \n";
+			wikidata.talktext += "else_hidden" +
+			    ((talks[prop].length > 0) ?
+			     talks[prop].length :
+			     "No") + "\n";
+			wikidata.talktext += "abstract_hidden : " +
+			    talks[prop].description + "\n";
+
+			wikidata.talktext += "speaker_hidden : " +
+			    users.first_name + last_name + "\n";
+
 			callback(null, wikidata);
 			console.log(wikidata.talktext);
 		    }
